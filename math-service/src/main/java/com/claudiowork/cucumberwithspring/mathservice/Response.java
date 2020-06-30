0e5ed1cd854004e30fb70685990b7510c31e9f7e
Claudio <claudiosfcarvalho@outlook.com>
2020-06-30 19:50:18 -0300

    atualizando

---
+++ math-service/src/main/java/com/claudiowork/cucumberwithspring/mathservice/Response.java
@@ -0,0 +1,24 @@
+package com.claudiowork.cucumberwithspring.mathservice;
+
+public class Response {
+
+	private int n1;
+	private int n2;
+	private int result;
+	public Response(int n1, int n2, int result) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+		this.result = result;
+	}
+	public int getN1() {
+		return n1;
+	}
+	public int getN2() {
+		return n2;
+	}
+	public int getResult() {
+		return result;
+	}
+	
+}
